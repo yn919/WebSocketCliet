d775816b12749a44c4cc6c9ff4629cb3136217eb
yn919 <nk19yu@gmail.com>
2022-05-24 11:29:15 +0900

    add libralies

---
+++ WebSocketCliet/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocketCliet.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}
--- WebSocketCliet/Views/MainWindow.xaml.cs
+++ WebSocketCliet/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WebSocketCliet
+namespace WebSocketCliet.Views
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
